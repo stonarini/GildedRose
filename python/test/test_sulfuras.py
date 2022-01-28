2cb2cfc27fd297c2d602e034314c6676ebe465a3
stonarini <stonarini@cifpfbmoll.eu>
2022-01-28 23:45:07 +0100

    test(Sulfuras): add unit test for Sulfura class

---
+++ python/test/test_sulfuras.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.mark.create
+def test_create_item():
+    sulfuras = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80)
+    assert "Sulfuras, Hand of Ragnaros" == sulfuras.get_name()
+    assert 0 == sulfuras.get_sell_in()
+    assert 80 == sulfuras.get_quality()
+
+
+@pytest.mark.update_quality
+def test_update_quality():
+    sulfuras = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80)
+    sulfuras.update_quality()
+    assert 0 == sulfuras.get_sell_in()
+    assert 80 == sulfuras.get_quality()
