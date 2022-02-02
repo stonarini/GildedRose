408838197bcdd69e055abf483a4a284ae77c1600
stonarini <stonarini@cifpfbmoll.eu>
2022-02-02 03:16:00 +0100

    feat(GildedRose): create GildedRose class with list of Updateables items

---
+++ java/src/main/java/edu/poniperro/GildedRose.java
@@ -0,0 +1,28 @@
+package edu.poniperro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.poniperro.domain.items.NormalItem;
+
+public class GildedRose {
+    private List<NormalItem> stock = new ArrayList<NormalItem>();
+
+    public GildedRose() {
+    }
+
+    void addItem(NormalItem item) {
+        this.stock.add(item);
+    }
+
+    List<NormalItem> inventory() {
+        return this.stock;
+    }
+
+    void updateQuality() {
+        for (int i = 0; i < this.stock.size(); i++) {
+            this.stock.get(i).updateQuality();
+        }
+    }
+
+}
