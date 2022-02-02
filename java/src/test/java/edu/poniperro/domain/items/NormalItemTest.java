4c6bf5f77df3436b31e6b6f0ffbd247316bf5bec
stonarini <stonarini@cifpfbmoll.eu>
2022-02-02 03:08:48 +0100

    test(NormalItem): create test for NormalItem class

---
+++ java/src/test/java/edu/poniperro/domain/items/NormalItemTest.java
@@ -0,0 +1,49 @@
+package edu.poniperro.domain.items;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NormalItemTest {
+	@Test
+	public void crearNormalItem() {
+
+		NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 20);
+		assertEquals("+5 Dexterity Vest", normal.getName());
+		assertEquals(10, normal.getSellIn(), 0);
+		assertEquals(20, normal.getQuality(), 0);
+	}
+
+	@Test
+	public void toStringTest() {
+		NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 20);
+		System.out.println(normal.toString());
+	}
+
+	@Test
+	public void updateQualityNormalItem() {
+
+		NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 20);
+		normal.updateQuality();
+		assertEquals(9, normal.getSellIn(), 0);
+		assertEquals(19, normal.getQuality(), 0);
+	}
+
+	@Test
+	public void updateQualityNormalItemExpired() {
+
+		NormalItem normal = new NormalItem("+5 Dexterity Vest", 0, 20);
+		normal.updateQuality();
+		assertEquals(-1, normal.getSellIn(), 0);
+		assertEquals(18, normal.getQuality(), 0);
+	}
+
+	@Test
+	public void qualityMinZERO() {
+
+		NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 0);
+		normal.updateQuality();
+		assertEquals(9, normal.getSellIn(), 0);
+		assertEquals(0, normal.getQuality(), 0);
+	}
+}
