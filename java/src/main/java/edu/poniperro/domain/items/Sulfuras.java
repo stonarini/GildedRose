97480c180076911a5e2460385b6643bce84ec22b
stonarini <stonarini@cifpfbmoll.eu>
2022-02-02 03:14:11 +0100

    feat(Sulfuras): add Sulfuras class, inheriting from NormalItem

---
+++ java/src/main/java/edu/poniperro/domain/items/Sulfuras.java
@@ -0,0 +1,11 @@
+package edu.poniperro.domain.items;
+
+public class Sulfuras extends NormalItem {
+	public Sulfuras(String name, int sellIn, int quality) {
+		super(name, sellIn, quality);
+	}
+
+	@Override
+	public void updateQuality() {
+	}
+}
